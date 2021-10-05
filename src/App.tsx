4436adc156d824f751c191211e340bce63dd3e89
Greta Prisby <greta.prisby@hitachivantara.com>
2021-10-04 20:23:04 -0400

    Init linting, typescript, and testing

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./assets/logo.gif";
+import "./App.css";
+
+const App: React.FC = () => (
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+    </header>
+  </div>
+);
+
+export default App;
---
+++ src/modules/Screens.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Screens: React.FC = () => {
+  return (
+    <div>This is the screens component</div>
+  );
+};
+
+export default Screens;
