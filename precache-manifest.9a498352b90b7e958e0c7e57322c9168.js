628a8668261b3267b892a125d7be0107e0255ab1
Greta Prisby <greta.prisby@hitachivantara.com>
2023-03-25 08:39:42 -0400

    Updates

---
+++ precache-manifest.9a498352b90b7e958e0c7e57322c9168.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e05016ba2dbf430cd830",
+    "url": "/rabbitype/static/css/main.4711839c.chunk.css"
+  },
+  {
+    "revision": "e05016ba2dbf430cd830",
+    "url": "/rabbitype/static/js/main.bce8d930.chunk.js"
+  },
+  {
+    "revision": "b9737f13401c5045552f",
+    "url": "/rabbitype/static/js/runtime~main.5e5a11d3.js"
+  },
+  {
+    "revision": "2cd1fbf3c8c87d07b820",
+    "url": "/rabbitype/static/js/2.496664a3.chunk.js"
+  },
+  {
+    "revision": "bf3f3b7061bd69c08914",
+    "url": "/rabbitype/static/js/3.333ed05c.chunk.js"
+  },
+  {
+    "revision": "9b02845792e197646fa68285813172bc",
+    "url": "/rabbitype/static/media/peaberry.9b028457.ttf"
+  },
+  {
+    "revision": "9e758c0045d4c8769eeebcad06f6625c",
+    "url": "/rabbitype/index.html"
+  }
+];
